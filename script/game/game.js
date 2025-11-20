3605bfb1d43d532889011959fb9511b5417d14e8
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-21 01:18:07 +0800

    Update game.js

--- script/game/game.js
+++ script/game/game.js
@@ -1114,11 +1114,11 @@ export default class Game {
 			input.getGameDown("hold")
 			)
 			) {
-				game.areLimit = 0
-				game.areLineLimit = 0
+				game.piece.areLimit = 0
+				game.piece.areLineLimit = 0
 			}
 			if (game.rotationSystem === "ds") {
-				game.areLimit = 0
+				game.piece.areLimit = 0
 			}
           }
           game.particle.update(msPassed)
